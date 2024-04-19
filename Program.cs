bc0f6f73a863a2750730e0e18b6d1e1ce22956ca
VaNkOSl <158475958+VaNkOSl@users.noreply.github.com>
2024-04-19 14:08:59 +0300

    The solution to the second шьян

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the first 9 digits of the ISBN: ");
+            string userInput = Console.ReadLine()!;
+
+            int checkDigit = CalculateCheckDigit(userInput);
+
+            string result;
+            if (checkDigit == 10)
+            {
+                result = "X";
+            }
+            else
+            {
+                result = checkDigit.ToString();
+            }
+
+            Console.WriteLine("The result ISBN is: " + userInput + result);
+        }
+
+        private static int CalculateCheckDigit(string isbnDigits)
+        {
+            int sum = 0;
+            for (int i = 0; i < isbnDigits.Length; i++)
+            {
+                int digit = int.Parse(isbnDigits[i].ToString());
+                sum += digit * (i + 1);
+            }
+
+            int checkDigit = sum % 11;
+
+            if (checkDigit == 10 )
+            {
+                return checkDigit;
+            }
+            else
+            {
+                return checkDigit;
+            }
+        }
+    }
+}
