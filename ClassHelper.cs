d37fdf460a042ec42b111c7d653d140c40fca0f3
VaNkOSl <158475958+VaNkOSl@users.noreply.github.com>
2024-04-19 14:15:51 +0300

    The solution to the third problem

---
+++ ClassHelper.cs
@@ -0,0 +1,43 @@
+﻿namespace Homework;
+public class ClassHelper
+{
+    public int[] RemoveDuplicates(int[] array)
+    {
+        int n = array.Length;
+        int[] uniqueArray = new int[n];
+        int index = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            bool isDuplicated = false;
+
+            for (int j = 0; j < i; j++)
+            {
+                if (array[i] == array[j])
+                {
+                    isDuplicated = true;
+                    break;
+                }
+            }
+
+            if (!isDuplicated)
+            {
+                uniqueArray[index++] = array[i];
+            }
+        }
+
+        Array.Resize(ref uniqueArray, index);
+
+        return uniqueArray;
+    }
+
+    public void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + "");
+        }
+        Console.WriteLine();
+    }   
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassHelper classHelper = new ClassHelper();
+
+            Console.WriteLine("Enter the number of element array");
+            int n = int.Parse(Console.ReadLine()!);
+
+            int[] originalArray = new int[n];
+
+            Console.WriteLine("Enter the element of array");
+            for (int i = 0; i < n; i++)
+            {
+                originalArray[i] = int.Parse(Console.ReadLine()!);
+            }
+
+            Console.WriteLine("Original Array:");
+            classHelper.PrintArray(originalArray);
+
+
+            int[] uniqueArray = classHelper.RemoveDuplicates(originalArray);
+            Console.WriteLine("Unique Array");
+
+            classHelper.PrintArray(uniqueArray);
+        }
+    }
+}
