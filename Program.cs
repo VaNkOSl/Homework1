77253a530481f80d53e1bdba8f5af4f04ff55479
VaNkOSl <158475958+VaNkOSl@users.noreply.github.com>
2024-04-22 12:50:21 +0300

    The solution of the first Task

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            ClassHelper helper = new ClassHelper();
+
+            Console.WriteLine("Please enter number A:");
+            int a = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Please enter number B:");
+            int b = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"Numbers with exactly two 'A' symbols in their duodecimal representation between {a} and {b}:");
+
+            for (int i = a; i <= b; i++)
+            {
+                string duodecimalRepresentation = helper.ToDuodecimal(i);
+                int countA = helper.CountA(duodecimalRepresentation);
+                if (countA == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
