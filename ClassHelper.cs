add061831536256cf0f5be3babe4d1be83a80cba
VaNkOSl <158475958+VaNkOSl@users.noreply.github.com>
2024-04-26 10:05:00 +0300

    Update ClassHelper.cs

--- ClassHelper.cs
+++ ClassHelper.cs
@@ -1,30 +1,6 @@
 ﻿namespace Homework;
 public class ClassHelper
 {
-        public static int[] RemoveDuplicates(int[] array)
-       {
-           Dictionary<int, bool> seen = new Dictionary<int, bool>();
-           List<int> uniqueList = new List<int>();
-
-           foreach (int num in array)
-           {
-               if (!seen.ContainsKey(num))
-               {
-                   uniqueList.Add(num);
-                   seen[num] = true;
-               }
-           }
-
-           return uniqueList.ToArray();
-       }
-
-    public void PrintArray(int[] array)
-    {
-        foreach (int element in array)
-        {
-            Console.Write(element + "");
-        }
-        Console.WriteLine();
-    }   
+       
 }
 
