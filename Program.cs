69257c0c1ff81bb0793e5ddc95647f66acf184a7
VaNkOSl <158475958+VaNkOSl@users.noreply.github.com>
2024-04-27 13:51:50 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,23 +11,23 @@
              Console.WriteLine("ISBN-10: " + isbn);
         }
 
-       public static string CalculateCheckDigit(string input)
-       {
-               int[] digits = new int[input.Length];
-               for (int i = 0; i < input.Length; i++)
-               {
-                   digits[i] = int.Parse(input[i].ToString());
-               }
-        
-               int sum = 0;
-               for (int i = 0; i < digits.Length; i++)
-               {
-                   sum += (i + 2) * digits[digits.Length - 1 - i];
-               }
-        
-               int checkDigit = (11 - (sum % 11)) % 11;
-        
-               return checkDigit == 10 ? "X" : checkDigit.ToString();
-       }
+      public static string CalculateCheckDigit(string input)
+      {
+          int[] digits = new int[input.Length];
+          for (int i = digits.Length - 1; i >= 0; i--)
+          {
+              digits[i] = int.Parse(input[i].ToString());
+          }
+
+          int sum = 0;
+          for (int i = 0; i < digits.Length; i++)
+          {
+              sum += (i + 2) * digits[digits.Length - 1 - i];
+          }
+
+          int checkDigit = (11 - (sum % 11)) % 11;
+
+          return checkDigit == 10 ? "X" : checkDigit.ToString();
+      }
     }
 }
